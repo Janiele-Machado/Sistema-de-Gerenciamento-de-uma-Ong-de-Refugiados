e0bf702b345a7a7dba07e0f7758798d057a8254f
Janiele-Machado <janiele.farias@estudante.ifgoiano.edu.br>
2024-11-09 20:04:23 -0300

    Criação do menu principal
    Criando o menu principal com as opções necessárias,e utilizando a estrutura de repetição do-while, para o menu ficar repetindo até o usuário queira sair.

--- ProjetoRefugiados/src/main/java/ong/App.java
+++ ProjetoRefugiados/src/main/java/ong/App.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ong;
 
-/**
- *
- * @author Janiele
- */
+import java.util.Scanner;
+
 public class App {
-    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int opc_principal;
+        do {
+            System.out.println("-----Menu de Opcoes---------");
+            System.out.println("1-Cadastro de usuarios");
+            System.out.println("2-Registrar doacoes");
+            System.out.println("3-Listagem de usuarios ");
+            System.out.println("4-Editar Cadastros");
+            System.out.println("5-Excluir Cadastros");
+            System.out.println("6-Sair");
+            opc_principal = scan.nextInt();
+            scan.nextLine();
+
+        } while (opc_principal != 6);
+    }
 }
