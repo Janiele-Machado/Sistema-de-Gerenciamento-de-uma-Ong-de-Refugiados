a97b561d395d9c014288581fe05f44319b216c33
samueloli22 <matheus.bastos@estudante.ifgoiano.edu.br>
2024-11-13 13:06:05 -0300

    criação dos atributos e construtor da classe usuarios
    essa classe vai ser uma classe pai

--- ProjetoRefugiados/src/main/java/ong/Usuario.java
+++ ProjetoRefugiados/src/main/java/ong/Usuario.java
@@ -2,4 +2,15 @@ package ong;
 
 public class Usuario {
     
+    
+    private String nome;
+    private String data_nasc;
+    private String nacionalidade;
+
+    public Usuario(String nome, String data_nasc, String nacionalidade){
+        this.nome = nome;
+        this.data_nasc = data_nasc;
+        this.nacionalidade = nacionalidade;
+    }
+    
 }
