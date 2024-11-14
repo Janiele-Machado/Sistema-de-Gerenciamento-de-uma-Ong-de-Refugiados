1b700fff17f1d54ece7d34defa36c0650d587864
samueloli22 <samuelantuneso@icloud.com>
2024-11-14 12:32:03 -0300

    começo da classe doadores
    crio os atributos e o construtor lembrando que essa classe e uma subclasse

--- ProjetoRefugiados/src/main/java/ong/Doadores.java
+++ ProjetoRefugiados/src/main/java/ong/Doadores.java
@@ -1,5 +1,17 @@
 package ong;
-
-public class Doadores {
+import java.time.LocalDate;
+public class Doadores extends Usuario {
+   private LocalDate data_doacoes;
+   private float valor;
+   private  int id;
+    
+            public Doadores(String nome, String data_nasc, String nacionalidade, LocalDate data_doacoes, float valor, int id){
+                super(nome,data_nasc,nacionalidade);
+                this.valor=valor;
+                this.data_doacoes= data_doacoes;
+                this.id=id;
+            }
+            
+    
     
 }
