154771b76f3303e5ad2a24c614d31aea7e79ddc0
samueloli22 <samuelantuneso@icloud.com>
2024-11-15 11:36:15 -0300

    "Adicionei funcionalidade para cadastrar múltiplos doadores com opção de continuidade
    Implementei um loop para interagir com o usuário, perguntando se ele deseja cadastrar um doador. Se a resposta for "sim" ou "s", o sistema continua pedindo para cadastrar outro doador. O loop só é interrompido quando o usuário responde "não" ou "n", encerrando o processo de cadastro.

--- ProjetoRefugiados/src/main/java/ong/App.java
+++ ProjetoRefugiados/src/main/java/ong/App.java
@@ -2,6 +2,7 @@ package ong;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import static javax.management.Query.or;
 
 public class App {
 
@@ -28,35 +29,40 @@ public class App {
                 opc_principal = Integer.parseInt(opct);
 
                 if (opc_principal == 1) {
-                    while(opc1!=4)
+                    while(opc1!=4){
                         try{
-                    System.out.println("ola digite qual tipo de usuario quer cadastrar");
-                    System.out.println("1 refugiado");
-                    System.out.println("2 voluntario");
-                    System.out.println("3 doadores");
-                    System.out.println("4 sair");                          
-                    String opc1t = scan.nextLine();
-                    opc1 = Integer.parseInt(opc1t);
-                    if(opc1==1){
+                            System.out.println("ola digite qual tipo de usuario quer cadastrar");
+                            System.out.println("1 refugiado");
+                            System.out.println("2 voluntario");
+                            System.out.println("3 doadores");
+                            System.out.println("4 sair");                          
+                            String opc1t = scan.nextLine();
+                            opc1 = Integer.parseInt(opc1t);
+                            if(opc1==1){
                         
                         
-                    }else if(opc1==2){
+                            }else if(opc1==2){
                         
-                    }else if(opc1==3){
-                        System.out.println("deseja cadastra um doador (S)im ou (N)ao");
-                        String s_n= scan.nextLine();
-                        while(s_n.equals("sim")){
+                            }else if(opc1==3){
+                                System.out.println("deseja cadastra um doador (S)im ou (N)ao");
+                                String s_n= scan.nextLine();
+                                while(s_n.equals("sim") || s_n.equals("s")){
                             
+                            
+                            
+                             System.out.println("deseja cadastra outro doador (S)im ou (N)ao");
+                             s_n= scan.nextLine();
+                                    
                         
-                    }
+                                }
                         
-                    }else{
-                        System.out.println("Erro, digite um numero valido");
+                            }else{
+                                System.out.println("Erro, digite um numero valido");
+                            }
+                        }catch(NumberFormatException e){
+                             System.out.println("Error, digite um numero inteiro valido, por favor");
+                        }
                     }
-                     }catch(NumberFormatException e) {
-                        System.out.println("Error, digite um numero inteiro valido, por favor");
-                     }
-                    
 
                 } else if (opc_principal == 2) {
 
