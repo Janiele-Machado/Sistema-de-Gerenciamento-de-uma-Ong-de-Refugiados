8700248aeb47748bf15bc7fe03ef4d93e16b32f4
samueloli22 <samuelantuneso@icloud.com>
2024-11-16 14:31:42 -0300

    Criação da classe Conexao para gerenciamento de conexão com o banco de dados MySQL
    Implementação da classe Conexao com um método para obter a conexão com o banco de dados MySQL, utilizando constantes de configuração para a URL do servidor, usuário e senha. O método getConexao realiza a conexão e retorna uma instância de Connection, com tratamento de exceção para capturar possíveis erros durante o processo. A senha foi omitida e deve ser configurada adequadamente.

--- SistemasRefugiados/src/main/java/ong/Conexao.java
+++ SistemasRefugiados/src/main/java/ong/Conexao.java
@@ -1,7 +1,24 @@
 
 package ong;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 
 public class Conexao {
+    public static final String SERVIDOR = "jdbc:mysql://localhost:3306/banco_refugiados";
+    public static final String USUARIO = "root";
+    public static final String SENHA = ...;
     
+    
+    public Connection getConexao() {
+        try {
+            return DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 }
+    
+    
+
