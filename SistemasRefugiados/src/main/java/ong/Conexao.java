a4d2079d5a612738613ecfa0e0260632f337f1b9
samueloli22 <samuelantuneso@icloud.com>
2024-11-16 17:55:16 -0300

    Update conexao
    coloco mensagem para verificar a conexao

--- SistemasRefugiados/src/main/java/ong/Conexao.java
+++ SistemasRefugiados/src/main/java/ong/Conexao.java
@@ -7,14 +7,18 @@ import java.sql.DriverManager;
 public class Conexao {
     public static final String SERVIDOR = "jdbc:mysql://localhost:3306/banco_refugiados";
     public static final String USUARIO = "root";
-    public static final String SENHA = ...;
+    public static final String SENHA = "....";
     
     
     public Connection getConexao() {
         try {
-            return DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
+            Connection conexao = DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
+            if (conexao != null) {
+                System.out.println("Conexão bem-sucedida com o banco de dados!");
+            }
+            return conexao;
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Erro ao conectar: " + e.getMessage());
         }
         return null;
     }
