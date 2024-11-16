e50f1f7aaa1ae4a39d25c9251e97188c3e68aa5a
samueloli22 <samuelantuneso@icloud.com>
2024-11-16 18:00:59 -0300

    refatoraçao e criaçao do metodo inserir na classe usuarios
    criei o método inserir() para registrar as informações de um usuário no banco de dados. A função estabelece uma conexão com o MySQL usando a classe Conexao e prepara uma instrução SQL para inserir os dados na tabela usuarios. Utilizei o PreparedStatement para substituir os parâmetros da query com os valores do objeto (nome, nacionalidade e data de nascimento). Após a execução do comando, o PreparedStatement é fechado. Agora, o código permite que as informações de um usuário sejam salvas de forma persistente no banco de dados, garantindo que os dados sejam registrados corretamente na tabela usuarios.

--- SistemasRefugiados/src/main/java/ong/Usuarios.java
+++ SistemasRefugiados/src/main/java/ong/Usuarios.java
@@ -1,18 +1,38 @@
 
 package ong;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 
 public class Usuarios {
       
-    private String nome;
-    private String data_nasc;
-    private String nacionalidade;
+    protected String nome;
+    protected String data_nasc;
+    protected String nacionalidade;
 
     public Usuarios(String nome, String data_nasc, String nacionalidade){
         this.nome = nome;
         this.data_nasc = data_nasc;
         this.nacionalidade = nacionalidade;
     }
-    
+    public void inserir() {
+//        grava um aluno no banco
+//        Conexao c = new Conexao();
+//        Connection conexao = c.getConexao();
+        Connection conexao = new Conexao().getConexao();
+        String sql = "INSERT INTO usuarios(nome, nacionalidade, Data_nasc) VALUES(?, ?, ?)";
+        try{
+             PreparedStatement comando = conexao.prepareStatement(sql);
+             comando.setString(1, this.nome);
+             comando.setString(2, this.nacionalidade);
+             comando.setString(3, this.data_nasc);
+             
+             comando.execute();
+             comando.close();
+            
+        }catch(Exception e){
+            System.out.println("e");
+        }
+    }
 } 
 
