07fd81670cd60d9fdc0bfef8ed94ab9c32f73462
Arthur Henrique <janiele.farias@estudante.ifgoiano.edu.br>
2024-11-21 13:09:16 -0300

    Atributos e método Construtor na Classe voluntários.
    Criação dos atributos específicos da classe Voluntários, e a implementação do método construtor dela

--- SistemasRefugiados/src/main/java/ong/Voluntarios.java
+++ SistemasRefugiados/src/main/java/ong/Voluntarios.java
@@ -1,6 +1,15 @@
 package ong;
 
+public class Voluntarios extends Usuarios {
 
-public class Voluntarios {
-    
+    private String email;
+    private String habilidades;
+
+    public Voluntarios(String nome, String data_nasc, String nacionalidade, String email, String habilidades) {
+        super(nome, data_nasc, nacionalidade);
+        this.email = email;
+        this.habilidades = habilidades;
+
+    }
+
 }
