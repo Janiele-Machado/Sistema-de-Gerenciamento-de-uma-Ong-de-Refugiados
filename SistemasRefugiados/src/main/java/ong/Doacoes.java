23867603ccdb74e80d2391c6c628eefc143e5476
samueloli22 <samuelantuneso@icloud.com>
2024-11-20 21:25:47 -0300

    Classe doacoes Criação de atributos e metodo construtor
    Criação da classe Doacoes com os atributos doacoes_tipo, doacoes_quant e doacoes_date. Implementação do construtor para inicializar os valores dessas variáveis, permitindo o armazenamento e manipulação das informações relacionadas às doações.

--- SistemasRefugiados/src/main/java/ong/Doacoes.java
+++ SistemasRefugiados/src/main/java/ong/Doacoes.java
@@ -1,8 +1,21 @@
 
 package ong;
 
+import java.time.LocalDate;
+
 
 public class Doacoes {
+   private String doacoes_tipo;
+   private String doacoes_quant;
+   private LocalDate doacoes_date;
+    
+   public  Doacoes(String doacoes_tipo, String doacoes_quant, LocalDate doacoes_date){
+       this.doacoes_tipo= doacoes_tipo;
+       this.doacoes_quant= doacoes_quant;
+       this.doacoes_date = doacoes_date;
+       
+   }
+    
     
     
 }
