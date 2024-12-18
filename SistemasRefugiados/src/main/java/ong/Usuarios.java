6f106bb2acd88d5b2d88226a2254570905b42b64
Matheus-fel <matheus.bastos@estudante.ifgoiano.edu.br>
2024-12-18 12:21:04 -0300

    Comentarios java doc Classe Usuarios
    Comentei a classe mae usuarios

--- SistemasRefugiados/src/main/java/ong/Usuarios.java
+++ SistemasRefugiados/src/main/java/ong/Usuarios.java
@@ -6,18 +6,48 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * A classe Usuarios é a classe base (ou mãe) que representa um usuário em um
+ * sistema que interage com um banco de dados. Ela fornece métodos para inserir
+ * e listar usuários, que podem ser estendidos por outras classes como Doadores,
+ * Refugiados e Voluntarios.
+ *
+ * As subclasses podem herdar atributos como nome, data de nascimento e
+ * nacionalidade, além dos métodos de inserção e listagem de usuários.
+ *
+ * @author Matheus
+ * @since 16/11/2024
+ */
 public class Usuarios {
 
     protected String nome;
     protected String data_nasc;
     protected String nacionalidade;
 
+    /**
+     * Construtor da classe Usuarios, utilizado para inicializar um novo objeto
+     * de usuário com nome, data de nascimento e nacionalidade.
+     *
+     * @param nome O nome do usuário.
+     * @param data_nasc A data de nascimento do usuário.
+     * @param nacionalidade A nacionalidade do usuário.
+     *
+     */
     public Usuarios(String nome, String data_nasc, String nacionalidade) {
         this.nome = nome;
         this.data_nasc = data_nasc;
         this.nacionalidade = nacionalidade;
     }
 
+    /**
+     * Insere um novo usuário no banco de dados. Este método executa uma
+     * inserção no banco de dados utilizando as informações fornecidas na
+     * criação do objeto. Não retorna nenhum valor.
+     *
+     * @throws Exception Se ocorrer algum erro durante a execução da operação de
+     * inserção no banco de dados.
+     *
+     */
     public void inserir() throws Exception {
 //        grava um usuario no banco
 //        Conexao c = new Conexao();
@@ -38,6 +68,14 @@ public class Usuarios {
         }
     }
 
+    /**
+     * Lista todos os usuários presentes no banco de dados. Este método executa
+     * uma consulta no banco de dados e imprime as informações de todos os
+     * usuários cadastrados. Não retorna nenhum valor.
+     *
+     * @throws SQLException Se ocorrer algum erro durante a execução da consulta
+     * no banco de dados.
+     */
     public void listar() throws SQLException {
         Connection conexao = new Conexao().getConexao();
         String sql_listar_uso = "SELECT * FROM  banco_refugiados.usuarios;";
