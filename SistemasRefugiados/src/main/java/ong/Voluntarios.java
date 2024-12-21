35675e18de1c2c276198c246e4b10c87177fcdde
Arthur Henrique <arthur.dias@estudante.ifgoiano.edu.br>
2024-12-21 00:34:48 -0300

    Adicionei os comentarios com java Docx

--- SistemasRefugiados/src/main/java/ong/Voluntarios.java
+++ SistemasRefugiados/src/main/java/ong/Voluntarios.java
@@ -8,38 +8,62 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Classe que representa os voluntários da ONG.
+ * Estende a classe {@link Usuarios}.
+ */
 public class Voluntarios extends Usuarios {
 
     private String email;
     private String habilidades;
     private int volu_id;
 
+    /**
+     * Construtor da classe Voluntarios.
+     *
+     * @param nome          Nome do voluntário.
+     * @param data_nasc     Data de nascimento do voluntário.
+     * @param nacionalidade Nacionalidade do voluntário.
+     * @param email         Email do voluntário.
+     * @param habilidades   Habilidades do voluntário.
+     */
     public Voluntarios(String nome, String data_nasc, String nacionalidade, String email, String habilidades) {
         super(nome, data_nasc, nacionalidade);
         this.email = email;
         this.habilidades = habilidades;
-
     }
 
+    /**
+     * Obtém o ID do voluntário.
+     *
+     * @return ID do voluntário.
+     */
     public int getVolu_id() {
         return volu_id;
     }
 
+    /**
+     * Define o ID do voluntário.
+     *
+     * @param volu_id ID do voluntário.
+     */
     public void setVolu_id(int volu_id) {
         this.volu_id = volu_id;
     }
 
+    /**
+     * Insere um novo voluntário no banco de dados.
+     * Realiza inserções nas tabelas `usuarios` e `voluntarios`.
+     *
+     * @throws SQLException Em caso de erro na execução da transação.
+     */
     @Override
     public void inserir() throws SQLException {
-        // conecta com o banco  
         Connection conexao = new Conexao().getConexao();
-        // Inserir usuário na tabela 'usuarios' (tabela genérica para usuários)
         String sqlUsuario = "INSERT INTO usuarios(nome, nacionalidade, Data_nasc) VALUES(?, ?, ?)";
-        //INSERT INTO banco_refugiados.voluntarios (volu_habilidades,volu_email, fk_usuarios_volu_id)VALUES ('conhecimento de informática', 'dol@gmail.com', '14');
         String sqlVoluntario = "INSERT INTO voluntarios (fk_usuarios_volu_id, volu_email, volu_habilidades) VALUES(?, ?, ?)";
 
         try {
-            // Começar uma transação para garantir consistência
             conexao.setAutoCommit(false);
 
             PreparedStatement comandoUsuario = conexao.prepareStatement(sqlUsuario, PreparedStatement.RETURN_GENERATED_KEYS);
@@ -48,41 +72,39 @@ public class Voluntarios extends Usuarios {
             comandoUsuario.setString(3, this.data_nasc);
             comandoUsuario.executeUpdate();
 
-            // Obter o id gerado automaticamente (caso o banco utilize auto incremento)
             ResultSet rs = comandoUsuario.getGeneratedKeys();
             rs.next();
             int voluntarios_id = rs.getInt(1);
 
-            // Inserir dados na tabela 'voluntarios'
             PreparedStatement comandoVoluntario = conexao.prepareStatement(sqlVoluntario);
             comandoVoluntario.setInt(1, voluntarios_id);
             comandoVoluntario.setString(2, this.email);
             comandoVoluntario.setString(3, this.habilidades);
             comandoVoluntario.executeUpdate();
 
-            // Confirmar a transação
             conexao.commit();
             System.out.println("Voluntario inserido com sucesso!");
 
-            // Fechar as conexões
             comandoUsuario.close();
             comandoVoluntario.close();
             conexao.setAutoCommit(true);
 
         } catch (Exception e) {
             try {
-                // Em caso de erro, desfaz a transação
                 conexao.rollback();
             } catch (SQLException rollbackEx) {
                 System.out.println("Erro ao reverter a transação: " + rollbackEx.getMessage());
             }
             System.out.println("Erro ao inserir Voluntario: " + e.getMessage());
-
         }
     }
 
+    /**
+     * Lista todos os voluntários cadastrados no banco de dados.
+     *
+     * @throws SQLException Em caso de erro ao acessar o banco de dados.
+     */
     public void listar() throws SQLException {
-
         Connection conexao = new Conexao().getConexao();
         String sqlListar = "SELECT *FROM usuarios join  voluntarios on usu_id = fk_usuarios_volu_id;";
         try {
@@ -104,7 +126,6 @@ public class Voluntarios extends Usuarios {
         } catch (SQLException e) {
             System.out.println(e);
         } finally {
-
             try {
                 if (conexao != null && !conexao.isClosed()) {
                     conexao.close();
@@ -112,11 +133,15 @@ public class Voluntarios extends Usuarios {
             } catch (SQLException e) {
                 System.out.println(e);
             }
-
         }
     }
 
-    //SOBRECARGA
+    /**
+     * Lista os dados de um voluntário com base no email informado.
+     *
+     * @param l_email Email do voluntário a ser listado.
+     * @throws SQLException Em caso de erro ao acessar o banco de dados.
+     */
     public void listar(String l_email) throws SQLException {
         this.email = l_email;
         Connection conexao = new Conexao().getConexao();
@@ -140,7 +165,6 @@ public class Voluntarios extends Usuarios {
         } catch (SQLException e) {
             System.out.println(e);
         } finally {
-
             try {
                 if (conexao != null && !conexao.isClosed()) {
                     conexao.close();
@@ -148,15 +172,20 @@ public class Voluntarios extends Usuarios {
             } catch (SQLException e) {
                 System.out.println(e);
             }
-
         }
-
     }
 
+    /**
+     * Verifica se um voluntário existe no banco de dados com o email e ID fornecidos.
+     *
+     * @param email_verificar Email a ser verificado.
+     * @param id_verificar    ID a ser verificado.
+     * @return {@code true} se o email e ID corresponderem a um voluntário existente,
+     *         {@code false} caso contrário.
+     * @throws SQLException Em caso de erro ao acessar o banco de dados.
+     */
     public boolean verificar(String email_verificar, int id_verificar) throws SQLException {
-
         Connection conexao = new Conexao().getConexao();
-        //SELECT fk_usuarios_volu_id  FROM voluntarios WHERE volu_email= "dol@gmail.com"
         String sqlVerificar = "SELECT fk_usuarios_volu_id  FROM voluntarios WHERE volu_email=? ";
         try {
             PreparedStatement comandoVerificar = conexao.prepareStatement(sqlVerificar);
@@ -184,7 +213,6 @@ public class Voluntarios extends Usuarios {
             return false;
 
         } finally {
-
             try {
                 if (conexao != null && !conexao.isClosed()) {
                     conexao.close();
@@ -192,18 +220,19 @@ public class Voluntarios extends Usuarios {
             } catch (SQLException e) {
                 System.out.println(e);
             }
-
         }
-
     }
 
+    /**
+     * Atualiza os dados de um voluntário existente no banco de dados.
+     *
+     * @throws SQLException Em caso de erro ao executar a atualização.
+     */
     public void alterar() throws SQLException {
         Connection conexao = new Conexao().getConexao();
         String sqlAlterar = "UPDATE  usuarios  SET nome =?, Data_nasc=?, nacionalidade=? WHERE  usu_id=?";
-        //UPDATE  voluntarios SET volu_email=?,volu_habilidades =? WHERE fk_usuarios_volu_id=?
         String sqlAlterarvolu = "UPDATE voluntarios SET volu_email=?,volu_habilidades =? WHERE fk_usuarios_volu_id=?";
         try {
-
             PreparedStatement comandoAlterar = conexao.prepareStatement(sqlAlterar);
             comandoAlterar.setString(1, this.nome);
             comandoAlterar.setString(2, this.data_nasc);
@@ -222,14 +251,17 @@ public class Voluntarios extends Usuarios {
 
         } catch (SQLException e) {
             System.out.println(e);
-
         }
     }
 
+    /**
+     * Gera um relatório de todos os voluntários e salva em um arquivo de texto.
+     *
+     * @throws SQLException Em caso de erro ao acessar o banco de dados.
+     */
     public void relatorio() throws SQLException {
         Connection conexao = new Conexao().getConexao();
         String sql_relatorio = "SELECT * FROM usuarios inner join voluntarios on usu_id = fk_usuarios_volu_id;";
-        // Caminho do arquivo onde o relatório será salvo
         String arquivo = "relatorio.txt";
         try (BufferedWriter escritor = new BufferedWriter(new FileWriter(arquivo, true))) {
             try {
@@ -243,38 +275,33 @@ public class Voluntarios extends Usuarios {
                     String nacionalidade1 = rsd.getString("nacionalidade");
                     String data = rsd.getString("Data_nasc");
                     String emaild = rsd.getString("volu_email");
-                    String habilidade = rsd.getString("volu_habilidades"); // com a bola
-                    // Escreve no arquivo
+                    String habilidade = rsd.getString("volu_habilidades");
                     escritor.write("----------------------------------------");
                     escritor.newLine();
-                    escritor.write("nome: " + nome );
+                    escritor.write("nome: " + nome);
                     escritor.newLine();
-                    escritor.write("email: " + emaild );
+                    escritor.write("email: " + emaild);
                     escritor.newLine();
                     escritor.write("data de nascimento: " + data);
                     escritor.newLine();
                     escritor.write("nacionalidade: " + nacionalidade1);
                     escritor.newLine();
-                    escritor.write("habilidade: " + habilidade );
+                    escritor.write("habilidade: " + habilidade);
                     escritor.newLine();
-                    escritor.write("id: " + id_txt );
+                    escritor.write("id: " + id_txt);
                     escritor.newLine();
-                    
-
                 }
                 escritor.write("----------------------------------------");
                 escritor.newLine();
 
             } catch (SQLException e) {
                 System.out.println(e);
-
             }
 
         } catch (IOException e) {
             System.out.println(e);
         } finally {
             try {
-                // Fecha a conexão com o banco de dados
                 if (conexao != null && !conexao.isClosed()) {
                     conexao.close();
                 }
@@ -283,5 +310,4 @@ public class Voluntarios extends Usuarios {
             }
         }
     }
-
 }
